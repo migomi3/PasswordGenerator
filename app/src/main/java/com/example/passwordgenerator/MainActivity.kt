8285596df419844de35a79f4c4b3daabe63a6903
migomi3 <migomi3@gmail.com>
2025-05-06 21:51:06 -0400

    added basic functionality and layout to create password

--- app/src/main/java/com/example/passwordgenerator/MainActivity.kt
+++ app/src/main/java/com/example/passwordgenerator/MainActivity.kt
@@ -1,48 +1,81 @@
 package com.example.passwordgenerator
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.SeekBar
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.passwordgenerator.ui.theme.PasswordGeneratorTheme
+import com.google.android.material.chip.Chip
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            PasswordGeneratorTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
+        setContentView(R.layout.main_activity)
 
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+        val seekBar: SeekBar = findViewById(R.id.seekBar)
+        val specialCharChip: Chip = findViewById(R.id.specialCharactersChip)
+        val numberChip: Chip = findViewById(R.id.numbersChip)
+        val capitalChip: Chip = findViewById(R.id.capitalChip)
+        val createPasswordButton: Button = findViewById(R.id.createPasswordButton)
+        val charCountView: TextView = findViewById(R.id.character_count)
+
+        seekBar.setMax(16)
+        seekBar.setOnSeekBarChangeListener(
+            object : SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(
+                    seekBar: SeekBar?,
+                    progress: Int,
+                    fromUser: Boolean
+                ) {
+                    charCountView.text = progress.toString()
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+                override fun onStopTrackingTouch(seekBar: SeekBar) {
+                    charCountView.text = seekBar.progress.toString()
                 }
             }
+        )
+
+        var password: String
+
+        createPasswordButton.setOnClickListener() {
+            password = GeneratePassword(seekBar.getProgress(), capitalChip.isChecked, numberChip.isChecked, specialCharChip.isChecked)
+
+            Toast.makeText(this, password, Toast.LENGTH_LONG).show()
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun GeneratePassword(length: Int, requiresCapitals: Boolean, requireNumbers: Boolean, requireSpecialCharacters: Boolean) : String {
+    val allowedChars: List<Char> =  ('a'..'z') + ('A'..'Z')
+
+    var password: String = List(length) { allowedChars.random()}.joinToString("")
+    if (!requiresCapitals) {
+        password.lowercase()
+    }
+
+    if (requireNumbers) {
+        password = replaceRandomCharWithNumber(length, password)
+    }
+    if (requireSpecialCharacters) {
+        password = replaceRandomCharWithSpecialChar(length, password)
+    }
+    
+    return password
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PasswordGeneratorTheme {
-        Greeting("Android")
-    }
+fun replaceRandomCharWithSpecialChar(length: Int, str: String) : String {
+    val specialCharacter: List<Char> = listOf('!', '@','$', '%', '*', '-', '_', '+', ':')
+    val indexToReplace: Int = Random.nextInt(0, length)
+    val newChar: Char = specialCharacter[Random.nextInt(0, specialCharacter.size)]
+    return str.replaceRange(indexToReplace, indexToReplace, newChar.toString())
+}
+
+fun replaceRandomCharWithNumber(length: Int, str: String) : String {
+    val indexToReplace: Int = Random.nextInt(0, length)
+    val numb: String = Random.nextInt(0,10).toString()
+    return str.replaceRange(indexToReplace, indexToReplace, numb)
 }
